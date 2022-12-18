ade8bef3df3ff480984c11fdd34db50a6cd003e6
NikolayTach <42199859+NikolayTach@users.noreply.github.com>
2022-12-17 22:26:48 -0800

    Add files via upload

---
+++ devoshake.js
@@ -0,0 +1,12 @@
+import from readerScale function NullRead0;
+import from BinaryJS function NullRead0;
+	{
+		read range view;
+	lack JMOD "string" JMOD ReaderScale.function
+	value :
+		[1];
+	reach JMOD "config_dev01" & "config_dev02";
+	[AVT mode/GSP mode]
+
+};
+
